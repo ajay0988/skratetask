e6b0660f866dddf09e8d7c3ac3fac3102184698d
123ajay0 <123ajay0@gmail.com>
2021-12-17 08:59:08 +0530

    1st V

--- controllers/meetingController.js
+++ controllers/meetingController.js
@@ -1,22 +1,37 @@
 const Meeting = require("../models/meeting")
 const User = require("../models/user")
-const mongoose = require("mongoose")
-// create new meeting
+
 
 exports.createMeeting = async (req, res, next) => {
     console.log(req.body)
 
-
+    // check for existence of users
     const { uid1, uid2, date } = req.body
 
-    const meeting = await Meeting.create({
-        uid1, uid2, date
-    })
-    return res.status(200).json({
-        MUID: meeting._id
+    try {
+       const  user1 = await User.findOne({ _id: uid1 })
+        const user2 = await User.findOne({ _id: uid2 })
+        console.log(user1,user2)
+        if (user1===null || user2==null) {
+            return res.status(404).json({
+                success: false,
+                message: "requested users not present"
+            })
+        }
+        const meeting = await Meeting.create({
+            uid1, uid2, date
+        })
+        return res.status(200).json({
+            UID: meeting._id
 
-    })
+        })
+    } catch (error) {
+        if (error.name==="CastError")
+        return res.status(500).json({
+            message: error.message
+        })
 
+    }
 }
 
 // get all meeting details
@@ -26,24 +41,26 @@ exports.getAllMeetings = async (req, res, next) => {
     const meetings = await Meeting.find({}).select('-__v')
     var meetlist = []
     meetings.forEach(async (v) => {
-        var meet={}
-        
+        var meet = {}
+
         meet["_id"] = v._id
-        
+
         var username1 = await User.findOne({ _id: v.uid1.toString() })
         username1 = username1.username
         var username2 = await User.findOne({ _id: v.uid2.toString() })
         username2 = username2.username
 
-        meet["user1"]=username1
-        meet["user2"]=username2
+        meet["user1"] = username1
+        meet["user2"] = username2
         // meet={_id,username1,username2}
-        // console.log(username1,username2)
+        console.log(meet)
+        // console.log(username1, username2)
         meetlist.push(meet)
+        console.log(meetlist.length)
     })
+// console.log(meetlist.length)
 
-
-res.status(200).json({
-    meetings: meetlist
-})
+    res.status(200).json({
+        meetings: meetlist
+    })
 }
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,38 +1,55 @@
-const User=require("../models/user")
+const User = require("../models/user")
 
 
 // register a USer
 
-exports.registerUser=async(req,res,next)=>{
-        console.log(req.body)
+exports.registerUser = async (req, res, next) => {
+    console.log(req.body)
 
-        
-        const {username}=req.body
-        if (username.length<6 || username.length>20){
-            return res.status(500).json({
-                message: "username's length must be grater than or equal to 6 and less than 20"
+
+    const { username } = req.body
+    if (username.length < 6 || username.length > 20) {
+        return res.status(500).json({
+            message: "username's length must be grater than or equal to 6 and less than 20"
+        })
+    }
+    // console.log(username.length)
+   
+    try {
+        const user = await User.findOne({ username })
+        // console.log(user)
+        if (user) {
+            return res.status(404).json({
+                success: false,
+                message: "username already exist"
             })
         }
-        // console.log(username.length)
-
-        const user=await User.create({
+        const createduser = await User.create({
             username
         })
+
         return res.status(200).json({
-            UID : user._id
+            UID: createduser._id
 
         })
-    
-    
+    } catch (error) {
+
+        if (error.code===11000)
+        return res.status(500).json({
+            message: error.message
+        })
     }
+
+
+}
 // get all users
-    exports.getAllUsers=async(req,res,next)=>{
+exports.getAllUsers = async (req, res, next) => {
 
-        const users=await User.find().select('-__v')
-        return res.status(200).json({
-            users
-           
+    const users = await User.find().select('-__v')
+    return res.status(200).json({
+        users
 
-        })
-    
-    }
+
+    })
+
+}
